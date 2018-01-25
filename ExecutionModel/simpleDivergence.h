65058c0110c96b91466f52ca69ad14ae255fbdc0
SolemnJoker <wenxiongkjsm@163.com>
2018-01-25 18:40:41 +0800

    update

--- ExecutionModel/simpleDivergence.h
+++ ExecutionModel/simpleDivergence.h
@@ -5,6 +5,9 @@ Description	: simpleDivergence头文件
 **************************************************************************/
 #ifndef SIMPLEDIVERGENCE_H_
 #define SIMPLEDIVERGENCE_H_
+/*
+线程束分化测试，在gtx 750和gtx 940m上测试这几种方法的分支效率都是100%，没有发生线程束分化
+*/
 void call_mat_kernel1(float*c, int size, int block_size);
 void call_mat_kernel2(float*c, int size, int block_size);
 void call_mat_kernel3(float*c, int size, int block_size);
